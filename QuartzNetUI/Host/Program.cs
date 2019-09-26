4d5aca8556e9795892145c3330866746a2adf45d
initial <initial@users.noreply.github.com>
2019-09-26 16:05:09 +0800

    Update Program.cs
    增加自定义网站端口

--- QuartzNetUI/Host/Program.cs
+++ QuartzNetUI/Host/Program.cs
@@ -17,9 +17,19 @@ namespace Host
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+       // public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+       //         .UseStartup<Startup>()
+      //          .Build();
+        
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+            return WebHost.CreateDefaultBuilder(args).UseConfiguration(config)
                 .UseStartup<Startup>()
                 .Build();
+        }
     }
 }
